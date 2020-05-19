40a8687a0827dc478383caebf3dde6cabad94c78
EZorkina <elena.zorkina@cengage.com>
2020-05-19 16:32:52 +0300

    Add two-impulsed flights, add time of flight

--- src/main/java/com/ubirayev/App.java
+++ src/main/java/com/ubirayev/App.java
@@ -2,6 +2,10 @@ package com.ubirayev;
 
 import java.awt.event.WindowEvent;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import javax.swing.*;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
@@ -20,6 +24,7 @@ public class App {
   private final Integer i1Default = 2;
   private final Integer i2Default = 2;
   private final Integer I = 51;
+  private final Double I_IN_RADIAN = Math.toRadians(I);
 
   private JPanel mainPanel;
   private JTextField textFieldr;
@@ -59,21 +64,69 @@ public class App {
     double R = Double.parseDouble(textFieldR.getText());
     double ra = Double.parseDouble(textFieldra.getText());
 
+    computeTwoImpulses(k, r, R);
+    computeThreeImpulses(k, r, R, ra);
+  }
+
+  private void computeTwoImpulses(double k, double r, double R) throws IOException {
+    BufferedWriter deltaVFile = new BufferedWriter(new FileWriter("TwoImpulses.xls"));
+    deltaVFile.write("\t ΔV1 \t ΔV2 \t ΔV \n");
+
+    twoImpulsesFirst(k, r, R, deltaVFile);
+    twoImpulsesSecond(k, r, R, deltaVFile);
+    twoImpulsesThird(k, r, R, deltaVFile);
+
+    double time = PI * pow(((R + r) / 2), 3.0 / 2.0) / sqrt(k);
+    deltaVFile.write("Time:" + "\t" + time);
+    deltaVFile.close();
+  }
+
+  private void computeThreeImpulses(double k, double r, double R, double ra) throws IOException {
     BufferedWriter deltaVFile = new BufferedWriter(new FileWriter("ThreeImpulses.xls"));
     deltaVFile.write("\t ΔV1 \t ΔV2 \t ΔV3 \t ΔV \n");
 
     threeImpulsesFirst(k, r, R, ra, deltaVFile);
     threeImpulsesSecond(k, r, R, ra, deltaVFile);
+
+    double time = PI * pow(((ra + r) / 2), 3.0 / 2.0) / sqrt(k) +
+        PI * pow(((ra + R) / 2), 3.0 / 2.0) / sqrt(k);
+    deltaVFile.write("Time:" + "\t" + time);
     deltaVFile.close();
+  }
+
+  private void twoImpulsesFirst(double k, double r, double R,
+      BufferedWriter deltaVFile) throws IOException {
+    double deltaV1 = sqrt(k / r) * (sqrt(2.0 * R / (R + r)) - 1.0);
+    double deltaV2 = sqrt(k / R + 2.0 * k * r / R / (R + r) - 2.0 * sqrt(k / R) * sqrt(2.0 * k * r / R / (R + r)) * cos(I_IN_RADIAN));
+    double deltaVsum = deltaV1 + deltaV2;
+    deltaVFile.write("First:" + deltaV1 + "\t" + deltaV2 + "\t" + deltaVsum + "\n");
+  }
+
+  private void twoImpulsesSecond(double k, double r, double R, BufferedWriter deltaVFile) throws IOException {
+    double deltaV1 = sqrt(k / r + 2 * k * R / r / (R + r) - 2 * sqrt(k / r) * sqrt(2 * k * R / r / (R + r)) * cos(I_IN_RADIAN));
+    double deltaV2 = sqrt(k / R) - sqrt(2.0 * k * r / R / (R + r));
+    double deltaVsum = deltaV1 + deltaV2;
+    deltaVFile.write("Second:" + deltaV1 + "\t" + deltaV2 + "\t" + deltaVsum + "\n");
+  }
+
+  private void twoImpulsesThird(double k, double r, double R, BufferedWriter deltaVFile) throws IOException {
+    final double I1 = toRadians(2.8071);
+      double deltaV1 = sqrt(
+          k / r + 2 * k * R / (r * (R + r)) - 2 * sqrt(k / r) * sqrt(2 * k * R / (r * (R + r)))
+              * cos(I1));
+      double deltaV2 = sqrt(
+          k / R + 2 * k * r / (R * (R + r)) - 2 * sqrt(k / R) * sqrt(2 * k * r / (R * (R + r)))
+              * cos(I_IN_RADIAN - I1));
+      double deltaVsum = deltaV1 + deltaV2;
+      deltaVFile.write("Third:" + "\t" + deltaV1 + "\t" + deltaV2 + "\t" + deltaVsum + "\n");
   }
 
   private void threeImpulsesFirst(double k, double r, double R, double ra,
       BufferedWriter deltaVFile) throws IOException {
-    double radian = Math.toRadians(I);
     double deltaV1 = sqrt(k / r) * (sqrt(2.0 * ra / (ra + r)) - 1.0);
     double Va1 = sqrt(k / ra) * sqrt((2.0 * r) / (ra + r));
     double Va2 = sqrt(k / ra) * sqrt((2.0 * R) / (ra + R));
-    double deltaV2 = sqrt(pow(Va1, 2) + pow(Va2, 2) - 2 * Va1 * Va2 * cos(radian));
+    double deltaV2 = sqrt(pow(Va1, 2) + pow(Va2, 2) - 2 * Va1 * Va2 * cos(I_IN_RADIAN));
     double deltaV3 = sqrt(k / R) * (sqrt((2.0 * ra) / (ra + R)) - 1.0);
     double deltaVsum = deltaV1 + deltaV2 + deltaV3;
     deltaVFile.write(
@@ -82,7 +135,6 @@ public class App {
 
   private void threeImpulsesSecond(double k, double r, double R, double ra,
       BufferedWriter deltaVFile) throws IOException {
-      double radian = Math.toRadians(I);
       double radian1 = Math.toRadians(Double.parseDouble(textFieldi1.getText()));
       double radian2 = Math.toRadians(Double.parseDouble(textFieldi2.getText()));
 
@@ -97,7 +149,7 @@ public class App {
       double Vk = sqrt(k / R);
       double Vp2 = sqrt(k / R) * (sqrt((2 * ra) / (ra + R)) - 1.0);
       double deltaV3 = sqrt(
-          pow(Vk, 2) + pow(Vp2, 2) - 2 * Vk * Vp2 * cos(radian - radian1 - radian2));
+          pow(Vk, 2) + pow(Vp2, 2) - 2 * Vk * Vp2 * cos(I_IN_RADIAN - radian1 - radian2));
       double deltaVsum = deltaV1 + deltaV2 + deltaV3;
       deltaVFile
           .write("Second:" + "\t" + deltaV1 + "\t" + deltaV2 + "\t" + deltaV3 + "\t" + deltaVsum + "\n");
